91896d5117d38b8c8d21c89a6d4a249b2b77c2e8
talhafortex <talha.mehmood@fortexsolutions.com.pk>
2026-03-19 22:51:36 +0500

    update post

--- server.js
+++ server.js
@@ -43,27 +43,8 @@ app.listen(8000, () => console.log("server is running on port 3000"));
 //     { id: 2, title: "this is title 2", desc: "this is description 2" },
 //     { id: 3, title: "this is title 3", desc: "this is description 3" }
 // ]
-//////////////get all posts with out db/////////////
-// app.get("/api/posts", (req, res) => {
-//     res.json(posts);
-// });
-//////////////get all posts with db//////////////////
-app.get("/api/posts", async (req, res) => {
-    try {
-        const result = await pool.query("SELECT * FROM posts ORDER BY id ASC");
-        res.json(result.rows);
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({ message: "Failed to fetch posts" });
-    }
-});
-////////////////////////////////////////////////////////////
 
-app.get("/api/posts/:id", (req, res) => {
-    const id = Number(req.params.id);
-    console.log("🚀 ~ id:", id)
-    res.json(posts.find((v) => v.id === id));
-});
+
 //////////////////// create post with out db/////////////////
 // app.post("/api/post", (req, res) => {
 //     const { id, title, desc } = req.body;
@@ -93,13 +74,79 @@ app.post("/api/post", async (req, res) => {
     }
 });
 
+//////////////get all posts with out db/////////////
+// app.get("/api/posts", (req, res) => {
+//     res.json(posts);
+// });
+//////////////get all posts with db//////////////////
+app.get("/api/posts", async (req, res) => {
+    try {
+        const result = await pool.query("SELECT * FROM posts ORDER BY id ASC");
+        res.json(result.rows);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: "Failed to fetch posts" });
+    }
+});
 
+///////////////get post by id without db///////////////////
 
+// app.get("/api/posts/:id", (req, res) => {
+//     const id = Number(req.params.id);
+//     console.log("🚀 ~ id:", id)
+//     res.json(posts.find((v) => v.id === id));
+// });
+///////////////get post by id with db///////////////////
+
+app.get("/api/posts/:id", async (req, res) => {
+    try {
+        const id = Number(req.params.id);
+        const result = await pool.query(
+            'SELECT * FROM posts WHERE id = $1',
+            [id]
+        );
+        if (result.rows.length === 0) {
+            return res.status(404).json({ message: "Post not found" });
+        }
+
+        res.status(200).json({
+            message: "Success",
+            date: result.rows[0],
+        });
+    } catch (e) {
+        console.error(e);
+        res.status(500).json({ message: "Failed to get post by id" });
+    }
+});
+
+///////////////update post by id with db///////////////////
+
+app.put("/api/posts/:id", async (req, res) => {
+    try {
+        const id = Number(req.params.id);
+        const {title, description}= req.body;
+        const result = await pool.query(
+            'UPDATE posts SET title = $1, description = $2 WHERE id = $3 RETURNING *',
+            [title, description, id]
+        );
+        if (result.rows.length === 0) {
+            return res.status(404).json({ message: "Post cannot be updated" });
+        }
+
+        res.status(200).json({
+            message: "post updated successfully",
+            data: result.rows[0],
+        });
+    } catch (e) {
+        console.error(e);
+        res.status(500).json({ message: "Failed to update post by id" });
+    }
+});
 
 ////////////////////////////////////////////////////////////
-app.delete("/api/post/delete/:id", (req, res)=>{
-    const id= Number(req.params.id);
-    
+app.delete("/api/post/delete/:id", (req, res) => {
+    const id = Number(req.params.id);
+
 })
 app.delete("/api/post/delete/:id", (req, res) => {
     const id = Number(req.params.id);
